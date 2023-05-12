c261f872bc2c5613b38cc59dd9a8295fa3e53288
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-05-12 15:55:49 +0900

    Display all student profiles when all checks are off.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import ProfileCard from "../components/ProfileCard";
 import { StudentData } from "../components/StudentData";
-import { CheckedItemsContext } from "../index";  // Import the Context
+import { CheckedItemsContext } from "../index";
 
 function Home() {
-    const checkedItems = useContext(CheckedItemsContext);  // Use the Context
+    const checkedItems = useContext(CheckedItemsContext);
 
     return (
         <div className="home">
@@ -14,8 +14,11 @@ function Home() {
                 const isClassChecked = checkedItems[item.class + '반'];
                 const isClubChecked = checkedItems[item.club];
 
-                // 모든 조건을 만족하는 학생만 반환
-                return isGradeChecked || isClassChecked || isClubChecked;
+                // checkedItems의 모든 값이 false인 경우, 모든 학생들을 표시
+                const allUnchecked = Object.values(checkedItems).every(value => value === false);
+
+                // 조건을 하나라도 만족하는 학생을 반환하거나, 모든 값이 체크 해제된 경우 모든 학생을 반환
+                return allUnchecked || isGradeChecked || isClassChecked || isClubChecked;
             }).map((item, index) => {
                 return (
                     <ProfileCard key={index} name={item.name} imageSrc={item.imageSrc} />
